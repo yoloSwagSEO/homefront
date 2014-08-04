8c4e01023bbfb2feb38373576df68c10e4479424
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-03 23:22:55 -0300

    Colony index and details initial commit

---
+++ view/colony_details.php
@@ -0,0 +1,32 @@
+<div class="content">
+	
+	<div class="titleBar">
+		<h1>Tellani Prime</h1>
+		<h2>Type: E-Class Planet</h2>
+	</div>
+	
+	<div class="grid">
+		<div class="row span1">
+			<div class="cell">
+				<img src="assets/images/planets/E.png" class="fleft">
+				<div class="fleft" style="width: 601px">
+					<table>
+						<tr>
+							<td class="caption">Planet Name</td>
+							<td>Tellani Prime</td>
+							<td class="caption">Class</td>
+							<td>E-Class (Terrestrial)</td>
+						</tr>
+						<tr>
+							<td class="caption">Star System</td>
+							<td>Tellani System</td>
+							<td class="caption">Coordinates</td>
+							<td>4:23 Alpha</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
---
+++ view/colony_index.php
@@ -0,0 +1,8 @@
+<div class="content">
+	
+	<div class="titleBar">
+		<h1>Colonies</h1>
+		<h2>Subtitle</h2>
+	</div>
+
+</div>

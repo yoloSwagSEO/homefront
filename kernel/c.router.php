db2057349daafb72bd65e76428941da2a8ccdb9f
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-05 19:39:18 -0300

    Class Router modified to include both action and ID

--- kernel/c.router.php
+++ kernel/c.router.php
@@ -4,7 +4,7 @@
 	#  HOMEFRONT ONLINE
 	## ---------------------------------------------------
 	#  Developed by Brunno Pleffken Hosti
-	#  File: c.core.php
+	#  File: c.router.php
 	#  Release: v1.0.0
 	#  Copyright: (c) 2014 - Homefront Interactive
 	## ---------------------------------------------------
@@ -15,13 +15,19 @@
 		// Get full URL to a page
 		// ---------------------------------------------------
 
-		public static function URL($controller, $id = "")
+		public static function URL($controller, $action = "", $id = 0)
 		{
-			if($id != "") {
-				$url = ROOT . "index.php?p=" . $controller . "&id=" . $id;
+			// Get controller and action
+			if($action != "") {
+				$url = ROOT . "index.php?p=" . $controller . "&action=" . $action;
 			}
 			else {
 				$url = ROOT . "index.php?p=" . $controller;
+			}
+
+			// Do we have an ID?
+			if($id != 0) {
+				$url = $url . "&id=" . $id;
 			}
 			
 			return $url;

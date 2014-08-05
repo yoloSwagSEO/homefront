e57f966304b3b10be2aca78b178ba1f2a281477f
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-05 17:50:27 -0300

    Basic Star Systems layout

--- view/colonies.php
+++ view/colonies.php
@@ -2,33 +2,43 @@
 	
 	<div class="titleBar">
 		<h1>Colonies</h1>
-		<h2>Subtitle</h2>
+		<h2>Star Systems</h2>
 	</div>
 	
 	<table>
 		<tr>
-			<td class="caption">Sort by</td>
-			<td class="caption">Filter</td>
+			<td class="caption">Star System</td>
+			<td class="caption">Population</td>
+			<td class="caption">Approval</td>
+			<td class="caption tcenter"><img src="assets/images/resources/metal-w.png"></td>
+			<td class="caption tcenter"><img src="assets/images/resources/deuterium-w.png"></td>
+			<td class="caption tcenter"><img src="assets/images/resources/industry-w.png"></td>
+			<td class="caption tcenter"><img src="assets/images/resources/food-w.png"></td>
+			<td class="caption tcenter"><img src="assets/images/resources/electricity-w.png"></td>
+			<td class="caption tcenter"><img src="assets/images/resources/research-w.png"></td>
+			<td class="caption">Hangar</td>
 		</tr>
 		<tr>
-			<td>
-				
-			</td>
-			<td>
-				Planet class:
-				<select>
-					<option>Class A (Planetoid)</option>
-					<option>Class B (Desertic)</option>
-					<option>Class C (Gas Giant)</option>
-					<option>Class D (Adaptable)</option>
-					<option>Class E (Terrestrial)</option>
-					<option>Class F (Artic)</option>
-					<option>Class G (Gas Supergiant)</option>
-					<option>Class H (Marginal)</option>
-					<option>Class I (Sulfuric)</option>
-					<option>Class J (Oceanic)</option>
-				</select>
-			</td>
+			<td>Tellani</td>
+			<td>1801 / 7500</td>
+			<td>92%</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">110 / 200</td>
+			<td class="tcenter">220 / 250</td>
+			<td class="tcenter">200</td>
+			<td>12 / 1 / 3</td>
+		</tr>
+		<tr>
+			<td class="tcenter" colspan="3">Total</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">100</td>
+			<td class="tcenter">110 / 200</td>
+			<td class="tcenter">220 / 250</td>
+			<td class="tcenter">200</td>
+			<td></td>
 		</tr>
 	</table>
 

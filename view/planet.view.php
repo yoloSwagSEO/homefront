7bf4f055f4f051aea7eaa13c394b2bd923aab925
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-05 23:20:16 -0300

    Planet/colony overview layout

---
+++ view/planet.view.php
@@ -0,0 +1,126 @@
+<div class="content">
+	
+	<div class="titleBar">
+		<h1>Planet: Tellani Prime</h1>
+		<h2>Type: M-Class Planet</h2>
+	</div>
+	
+	<div class="grid">
+		<div class="row span1">
+			<div class="cell">
+				<img src="assets/images/planets/E.png" class="fleft">
+				<div class="fleft" style="width: 601px">
+					<table>
+						<tr>
+							<td class="caption">Planet Name</td>
+							<td>Tellani Prime</td>
+							<td class="caption">Class</td>
+							<td>M-Class (Terrestrial)</td>
+						</tr>
+						<tr>
+							<td class="caption">Star System</td>
+							<td>Tellani System</td>
+							<td class="caption">Coordinates</td>
+							<td>4:23 Alpha</td>
+						</tr>
+					</table>
+					
+					<h3>Nominal Output</h3>
+					
+					<table>
+						<tr>
+							<td class="caption">Metal</td>
+							<td><img src="assets/images/resources/metal-w.png" class="icon fleft"> 100</td>
+							<td class="caption">Deuterium</td>
+							<td><img src="assets/images/resources/deuterium-w.png" class="icon fleft"> 100</td>
+							<td class="caption">Industry</td>
+							<td><img src="assets/images/resources/industry-w.png" class="icon fleft"> 100</td>
+						</tr>
+						<tr>
+							<td class="caption">Food</td>
+							<td><img src="assets/images/resources/food-w.png" class="icon fleft"> 100 / 150</td>
+							<td class="caption">Electricity</td>
+							<td><img src="assets/images/resources/electricity-w.png" class="icon fleft"> 100 / 210</td>
+							<td class="caption">Research</td>
+							<td><img src="assets/images/resources/research-w.png" class="icon fleft"> 100</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="grid">
+		<div class="row span2">
+			<div class="cell">
+				<h3>Demographic Info</h3>
+				<table>
+					<tr>
+						<td class="caption">Current Population</td>
+					</tr>
+					<tr>
+						<td>1100 / 7500</td>
+					</tr>
+					<tr>
+						<td class="caption">Population Changes</td>
+					</tr>
+					<tr>
+						<td>
+							Population Gained: +2<br>
+							Population Lost: -1<br>
+							Net Population Change: +1
+						</td>
+					</tr>
+				</table>
+				<h3>Planetary Structures</h3>
+			</div>
+			<div class="cell">
+				<h3>Economics</h3>
+				<table>
+					<tr>
+						<td class="caption">Revenue</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="fleft">
+								GDP Local:<br>
+								Space Commuters:<br>
+								System Trade:<br>
+								Interstellar Trade:<br>
+								Tax Base:<br>
+								Off-World Income:<br>
+								Expenses:<br>
+							</div>
+							<div class="fright">
+								Tax Rates<br>
+								Planetary:<br>
+								System:<br>
+								Sector:<br>
+								Imperial:<br><br>
+								Total Tax Burden = 10%
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="caption">Expenses</td>
+					</tr>
+					<tr>
+						<td>
+							Government Corruption:<br>
+							Security:<br>
+							Economic Development:<br>
+							Education:<br>
+							Social:<br>
+							Environmental:<br>
+							Research:<br>
+							Nearby Development:<br>
+							Debt Reduction:
+						</td>
+					</tr>
+				</table>
+				<h3>Military Info</h3>
+			</div>
+		</div>
+	</div>
+
+</div>

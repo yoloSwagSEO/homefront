803162241e1c40f80086cefd72f062a8948fe4d5
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-05 23:18:52 -0300

    Stellar system overview layout

---
+++ controller/system.php
@@ -0,0 +1,15 @@
+<?php
+
+	## ---------------------------------------------------
+	#  HOMEFRONT ONLINE
+	## ---------------------------------------------------
+	#  Developed by Brunno Pleffken Hosti
+	#  File: system.php
+	#  Release: v1.0.0
+	#  Copyright: (c) 2014 - Homefront Interactive
+	## ---------------------------------------------------
+
+	// Load Cycle2 plugin
+	$this->head = '<script src="../assets/js/jquery.cycle2.min.js" type="text/javascript"></script>';
+
+?>
---
+++ view/system.view.php
@@ -0,0 +1,110 @@
+<div class="content">
+	
+	<div class="titleBar">
+		<h1>Stellar System</h1>
+		<h2>Tellani System</h2>
+	</div>
+	
+	<table>
+		<tr>
+			<td class="caption">Population</td>
+			<td></td>
+			<td class="caption">Resources</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td class="caption">Approval</td>
+			<td></td>
+			<td class="caption">Trade</td>
+			<td></td>
+		</tr>
+	</table>
+	
+	<h3>System Overview</h3>
+	
+	<div style="overflow: hidden">
+		<!-- 1st PLANET -->
+		<div class="fleft" style="margin-right: 2px">
+			<table>
+				<tr><td colspan="2"><img src="assets/images/planets/E.png" width="165"></td></tr>
+				<tr><td colspan="2" class="tcenter">Tellani Prime<br>Terrestrial</td></tr>
+				<tr>
+					<td class="caption">Population</td>
+					<td>2200 / 7500</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/metal-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/deuterium-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/industry-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/food-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/electricity-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/research-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						
+					</td>
+				</tr>
+			</table>
+		</div>
+		
+		<!-- 2nd PLANET -->
+		<!-- 1st PLANET -->
+		<div class="fleft">
+			<table>
+				<tr><td colspan="2"><img src="assets/images/planets/A.png" width="165"></td></tr>
+				<tr><td colspan="2" class="tcenter">Tellani II<br>Geoinactive</td></tr>
+				<tr>
+					<td class="caption">Population</td>
+					<td>Unhabitable</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/metal-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/deuterium-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/industry-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/food-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/electricity-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						<div class="fix">
+							<div class="fleft" style="margin-right: 10px"><img src="assets/images/resources/research-w.png"></div>
+							<div class="fleft">200</div>
+						</div>
+						
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+</div>

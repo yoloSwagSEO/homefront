1934ac2974f55579df5e713f275ae48f7711a6b4
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-08-18 14:33:13 -0300

    New progress bar element

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,4 +58,27 @@ $(document).ready(function() {
 		});
 	});
 
+	/**
+	 * Build scrollbars
+	 */
+	
+	$('.progressBar').each(function() {
+		var minimum  = $(this).data('min'),
+		    maximum  = $(this).data('max'),
+		    current  = $(this).data('current'),
+		    barWidth = $(this).innerWidth(),
+		    diff, percentage, pointer;
+
+		diff = maximum - minimum;
+
+		if(current > maximum) {
+			current = maximum;
+		}
+
+		percentage = (100 / diff) * current;
+		pointer = Math.floor((barWidth / diff) * current);
+
+		$(this).append('<div class="pointer" style="left: ' + pointer + 'px"></div>');
+	});
+
 });

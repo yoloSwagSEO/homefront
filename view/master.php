3fdf7dbc7f898b2aa433f6a60899ee4847557cdb
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-09-16 17:31:59 -0300

    Rename jQuery file for easier version maintenance
    Update from v1.11.0 to v1.11.1

--- view/master.php
+++ view/master.php
@@ -7,7 +7,7 @@
 	<link href="assets/css/main.css" rel="stylesheet" type="text/css">
 	<link href="assets/css/perfect-scrollbar.min.css" rel="stylesheet" type="text/css">
 	<!-- JS PLUGINS -->
-	<script src="assets/js/jquery-1.11.0.min.js" type="text/javascript"></script>
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
 	<script src="assets/js/perfect-scrollbar.min.js" type="text/javascript"></script>
 	<script src="assets/js/perfect-scrollbar.with-mousewheel.min.js" type="text/javascript"></script>
 	<!-- CUSTOM HEADER -->

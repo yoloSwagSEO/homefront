b28132429cc8bf09173b1a0d83f683d3caf7ad99
Brunno Pleffken <brunno.pleffken@outlook.com>
2014-10-13 18:12:55 -0300

    Adjustments on configuration file

--- config.php
+++ config.php
@@ -9,12 +9,14 @@
 	#  Copyright: (c) 2014 - Homefront Interactive
 	## ---------------------------------------------------
 
-	// Timezone settings
+	// Set default configuration
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
 
+	// Set default timezone
 	date_default_timezone_set("GMT");
 
 	// Database variables
-
 	$config['db_server'] = "localhost";
 	$config['db_user'] = "root";
 	$config['db_password'] = "root";
